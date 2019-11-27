6fab8fb587bda607ca99d409072bfddd64319dae
Noah Olsson <noaholsson@hotmail.com>
2019-11-27 08:29:17 +0100

    Updated dependencies and added update functionality.

--- qterest.php
+++ qterest.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: QTE Rest
  * Description: QTE Rest adds new endpoints for the Wordpress API
- * Version: 1.2.0
+ * Version: 1.0.0
  * Author: QTE Development AB
  * Author URI: https://getqte.se/
  * Domain Path: /languages
@@ -12,8 +12,6 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('QTEREST_VERSION', '1.1.1');
-
 define('QTEREST_PLUGIN_DIR', plugin_dir_url(__FILE__));
 define('QTEREST_PLUGIN_PATH', plugin_dir_path(__FILE__));
 
@@ -22,3 +20,10 @@ require_once QTEREST_PLUGIN_PATH . 'includes/load.php';
 
 $qterest_controller = new QTEREST\REST_Controller\REST_Controller;
 $qterest_controller->hook_rest_server();
+
+// Setting up update
+$qterestUpdateChecker = Puc_v4p8_Factory::buildUpdateChecker(
+    'https://github.com/qteab/qterest/',
+    __FILE__,
+    'qterest'
+);
