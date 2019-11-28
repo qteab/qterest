13965e3ed51d1552a690d06ac50989d4fccbd1cc
Noah Olsson <noaholsson@hotmail.com>
2019-11-28 12:44:53 +0100

    Fixed a bug causing the css for forms not to load if only Mailchimp was activated

--- includes/enqueues.php
+++ includes/enqueues.php
@@ -18,7 +18,6 @@ global $qterest_settings;
 if ( $qterest_settings['contact'] ) {
 	function load_contact_scripts( $hook ) {
 		wp_enqueue_script( 'qterest-form', QTEREST_PLUGIN_DIR . '/assets/js/form.js', array( 'jquery', 'wp-api' ), '1.1.0', true );
-		wp_enqueue_style( 'qterest-form', QTEREST_PLUGIN_DIR . '/assets/css/form.css', false, '1.1.0' );
 	}
 	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\load_contact_scripts' );
 }
@@ -28,6 +27,13 @@ if ( $qterest_settings['mailchimp'] ) {
 		wp_enqueue_script( 'qterest-mailchimp', QTEREST_PLUGIN_DIR . '/assets/js/mailchimp.js', array( 'jquery', 'wp-api' ), '1.1.0', true );
 	}
 	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\load_mailchimp_scripts' );
+}
+
+if( $qterest_settings['mailchimp'] || $qterest_settings['contact'] ) {
+    function load_shared_scripts( $hook ) {
+        wp_enqueue_style( 'qterest-form', QTEREST_PLUGIN_DIR . '/assets/css/form.css', false, '1.1.0' );
+    }
+    add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\load_shared_scripts' );
 }
 
 function load_global_scripts( $hook ) {
