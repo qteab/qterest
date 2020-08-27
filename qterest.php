4eea4cd4042332d700d59ad4d841e1b9b706f16d
Noah Olsson <noaholsson@hotmail.com>
2020-08-27 11:50:53 +0200

    Changed to version 1.4.11

--- qterest.php
+++ qterest.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: QTE Rest
  * Description: QTE Rest adds new endpoints for the WordPress API
- * Version: 1.4.10
+ * Version: 1.4.11
  * Author: QTE Development AB
  * Author URI: https://getqte.se/
  * Domain Path: /languages
